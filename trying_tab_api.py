9eceda29caa2015d5a32df7e555defb4a1e23400
kothasri <85645128+Peekar@users.noreply.github.com>
2024-06-24 13:28:13 -0500

    clean up

--- trying_tab_api.py
+++ trying_tab_api.py
@@ -1,16 +1,17 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 from scrapers import *
 
 
-def login(username, password):
+def login(payload):
     endpoint = 'https://www.tabroom.com/user/login/login_save.mhtml'
-    payload = {'username': username, 'password': password}
     return requests.post(url=endpoint, data=payload)
 
 
 def pairing_from_account_login():
     pass
+
 
 # returns array of the paragraphs in the html
 def get_judge_paradigm(judge_person_id, text=False):
@@ -30,13 +31,5 @@ def get_judge_id(first_name, last_name, middle_name=''):
     pass
 
 
-URL = 'https://www.tabroom.com/index.mhtml'
-payload = {
-    'value': 262656
-}
-
-r = requests.post(URL, data=payload)
-print(r.text)
-
-x = get_judge_paradigm(12312, False) # "hello"
-print(x) # print "hello"
+for el in html_table_to_dict_list(login({'username': os.environ['TABROOM_USERNAME'], 'password': os.environ['TABROOM_PASSWORD']}).text, html=True):
+    print(el, '\n')
